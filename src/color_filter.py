8f14e31f17de0d90fdda6415c285f964cf38d6e5
Heo Joon Woo <hensher5050@gmail.com>
2025-09-18 16:02:21 +0900

    make Kernels with np.array

--- src/color_filter.py
+++ src/color_filter.py
@@ -6,7 +6,14 @@ import matplotlib.pyplot as plt
 
 class Filters:
     # TODO: Image kernels
-    Kernels = {
+    Kernels = {"Original" : np.array([[0,0,0], [0,1,0], [0,0,0]], dtype=np.float32),
+               "Blur" : np.array([[1,1,1], [1,1,1], [1,1,1]], dtype=np.float32) / 9,
+               "Gaussian blur" : np.array([[1,2,1], [2,4,2], [1,2,1]], dtype=np.float32) / 16,
+               "Sharpen" : np.array([[0,-1,0], [-1,5,-1], [0,-1,0]], dtype=np.float32),
+               "Sobel (x)" : np.array([[-1,0,1], [-2,0,2], [-1,0,1]], dtype=np.float32),
+               "Sobel (y)" : np.array([[-1,-2,-1], [0,0,0], [1,2,1]], dtype=np.float32),
+               "Edge detection" : np.array([[-1,-1,-1], [-1,8,-1], [-1,-1,-1]], dtype=np.float32),
+               "Emboss" : np.array([[-2,-1,0], [-1,1,1], [0,1,2]], dtype=np.float32)
     }
 
     def __init__(self, kernels=Kernels):
